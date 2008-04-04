d3d855d50d7f1dbc7220090af0509379180d310b
manu <manu@5fc3e0e6-29bc-4d03-b52b-c088cb822bde>
2008-04-04 09:23:14 +0000

    git-svn-id: http://svn.parisson.org/svn/CNAQ/trunk@160 5fc3e0e6-29bc-4d03-b52b-c088cb822bde

--- tools/RI_FT.m
+++ tools/RI_FT.m
@@ -6,11 +6,15 @@ function [ri,ft]=RI_FT(f, sig_exc, sig_mes, f_s)
 DC = mean(sig_mes,1);
 sig_mes = sig_mes - DC;
 len_f = length(f);
-% f0 = f(1);
-% f1 = f(length(f));
+f0 = f(1);
+f1 = f(len_f);
 
 % Methode du produit
 siginv=fliplr(sig_exc)./f;
 ft=fft([sig_mes; zeros(len_f-1,1)]).*fft([siginv; zeros(len_f-1,1)]);
 ri=real(ifft(ft));
-
+norm=sqrt(sum(abs(ri.^2))/siz);
+yeff=norm*sqrt(f_s/2/(f1-f0));
+scal=f_s/yeff;
+ri=ri*scal;
+ft=ft/yeff;
